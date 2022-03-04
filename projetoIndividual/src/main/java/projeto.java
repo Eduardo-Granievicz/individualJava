baa7e6be943ab16aada9ff20ddc6386cd7f938b7
eduardo-granievicz <eduardo.moreno@bandtec.com>
2022-03-04 16:08:40 -0300

    Projeto Finalizado

--- projetoIndividual/src/main/java/projeto.java
+++ projetoIndividual/src/main/java/projeto.java
@@ -6,13 +6,30 @@ public class projeto {
     public static void main(String[] args) {
 
         Scanner leitor = new Scanner(System.in);
+         
+        Boolean continuarPrograma = true;
+        System.out.println(" -------------------------- ");
+        System.out.println("| Seja bem vindo ao CS.NET | ");
+        System.out.println(" -------------------------- ");
+        do {         
         
         
-        System.out.println("Seja bem vindo a StefGames!");
-        System.out.println("Por favor, digite um número");
+        System.out.println("\nPor favor, digite um número de 1 a 4"
+                         + "\n1 - Analisando inventário"
+                         + "\n2 - Vendo quantidade de campeonatos"
+                         + "\n3 - Sorteio de skins"
+                         + "\n4 - Sair do programa");
+        
+        
         Integer numeroDigitado = leitor.nextInt();
         
-
+            while (numeroDigitado > 4) {                
+                if (numeroDigitado > 4){
+                    System.out.println("Favor inserir um número válido");
+                   
+            }
+            }
+        
         switch (numeroDigitado) {
                 case 1:
                 System.out.println("Quantas skins de AK-47 você tem?");
@@ -22,11 +39,25 @@ public class projeto {
                 System.out.println("Quantas skins de M4A4 você tem?");
                 Integer M4A4 = leitor.nextInt();
                 Integer total = (ak47 + AWP + M4A4);
-                System.out.println(String.format("Meu Deus, você tem um total de"
-                 + " %d skins", total));
-                
-                
+                if(total == 0){
+                System.out.println(String.format("Que isso, como você tem %d skins?"
+                                               + "\nEstá precisando adquirir algumas!", total));                
+                }
+                else if (total >= 1 && total <= 4){
+                System.out.println(String.format("%d é um número pouco de skins, mas com o tempo você"
+                                               + "\nvai adquirindo mais!", total));
+                }
+                else if (total >= 5 && total <= 9){
+                System.out.println(String.format("Caramba! Com essa quantidade de %d skins"
+                                               + "\njá está se tornando um inventário legal!", total));
+                }
+                else{
+                System.out.println(String.format("Meu Deus, você tem o invetário"
+                                               + "\ndos sonhos de qualquer jogador"
+                                               + "\ncom esse número de %d skins!", total));
+} 
                 break;
+
                 case 2:
                 System.out.println("Você já foi para quantos campeonatos de CSGO?");
                 Integer camps = leitor.nextInt();
@@ -43,34 +74,41 @@ public class projeto {
                 System.out.println("Me passe essa formula do dinheiro urgentemente!!!");
                 }
                 break;
+
+
                 case 3:
                 System.out.println("Bem vindo a roleta, vamos testar sua sorte!");
                 Integer sorte = ThreadLocalRandom.current().nextInt(1, 11);
                 System.out.println("Insira um número de 1 a 10");
                 Integer numeroSorteio = leitor.nextInt();
-                if (numeroSorteio <= 0 || numeroSorteio > 10){
-                System.out.println("Insira um numero válido");
-                }
-                else if (numeroSorteio > 0 && numeroSorteio <= 10){
+                
+                if (numeroSorteio > 0 && numeroSorteio <= 10){
                 System.out.println(String.format("Número sorteado foi o %d", sorte));
                 }
-                else if (sorte == numeroSorteio){
+                if (sorte == numeroSorteio){
                 System.out.println("Parabéns! Você ganhou uma skin muito valiosa");
                 }
-                else if (sorte != numeroSorteio) {
+                if (sorte != numeroSorteio) {
                 System.out.println("Não foi dessa vez! Tente novamente");
-                }
-                    
+                }    
                 break;
+
+
                 case 4:
                 System.out.println("Obrigado por utilizar o sistema!");
+                continuarPrograma = false;
+ 
                 break;
                 default:
                 System.out.println("Numero inválido");
                 System.out.println("Digite um número de 1 a 4");
                 Integer valido = leitor.nextInt();
+
+                
                
                 break;
-  }
-}
-}
+               
+                }
+              } while (continuarPrograma);
+             }
+            }
